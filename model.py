2761fab3266b9f031a07c1050b58d8dd97030f41
Mithun Parab <mithunprb@gmail.com>
2025-12-21 07:01:34 +0530

    updaed ./train_ddp.py and ./model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class MobileCLIPRanker(nn.Module):
         full_model, _, _ = mobileclip.create_model_and_transforms(model_name, pretrained=pretrained_path)
         
         self.backbone = full_model.image_encoder
-
+        
         for param in self.backbone.parameters():
             param.requires_grad = False
             
@@ -18,7 +18,6 @@ class MobileCLIPRanker(nn.Module):
             if any(x in name for x in ['head', 'projector', 'fc', 'layer_scale', 'norm']):
                 param.requires_grad = True
                 parameters_to_train.append(name)
-            
             if len(parameters_to_train) > 10: 
                 break
         
@@ -29,12 +28,21 @@ class MobileCLIPRanker(nn.Module):
             
         self.score_head = nn.Linear(dim, 1, bias=False)
 
+    def train(self, mode=True):
+        """
+        Critical Override:
+        When train() is called, we keep the backbone in eval mode 
+        to freeze BatchNorm statistics.
+        """
+        super().train(mode)
+        if mode:
+            self.backbone.eval()
+        return self
+
     def forward(self, x):
         self.backbone.eval()
         
         features = self.backbone(x)
-        
         features = features / features.norm(dim=-1, keepdim=True)
-        
         score = self.score_head(features)
         return score
