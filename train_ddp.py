f508e894524d386816cc4e3b43d8ba487f630a7b
Mithun Parab <mithunprb@gmail.com>
2025-12-21 06:47:03 +0530

    updaed ./train_ddp.py and ./model.py

--- train_ddp.py
+++ train_ddp.py
@@ -128,7 +128,7 @@ def main():
         )
         
         model = MobileCLIPRanker(model_name=MODEL_NAME).to(device)
-        model = DDP(model, device_ids=[local_rank], output_device=local_rank)
+        model = DDP(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=True)
         
         optimizer = optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=LR)
         
