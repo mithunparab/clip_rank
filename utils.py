1f1eac0468110b4ddb198a1887338e19ef4f2b6c
Mithun Parab <mithunprb@gmail.com>
2025-12-24 01:14:22 +0530

    update listwise val

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 import yaml
 from types import SimpleNamespace
 
+def recursive_namespace(d):
+    if isinstance(d, dict):
+        for k, v in d.items():
+            d[k] = recursive_namespace(v)
+        return SimpleNamespace(**d)
+    return d
+
 def load_config(path="config.yml"):
     with open(path, 'r') as f:
         cfg_dict = yaml.safe_load(f)
-    
-    cfg = SimpleNamespace(**cfg_dict)
-    cfg.system = SimpleNamespace(**cfg_dict['system'])
-    cfg.data = SimpleNamespace(**cfg_dict['data'])
-    cfg.model = SimpleNamespace(**cfg_dict['model'])
-    cfg.train = SimpleNamespace(**cfg_dict['train'])
-    return cfg
+    return recursive_namespace(cfg_dict)
