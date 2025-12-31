7aa6b03069991ef9713faabf73e14554c4c29fde
Mithun Parab <mithunprb@gmail.com>
2025-12-31 11:04:34 +0530

    updated train_ddp.py

--- model.py
+++ model.py
@@ -14,13 +14,13 @@ class MobileCLIPRanker(nn.Module):
         self.backbone = model.image_encoder
         self.backbone_dim = 512 
         
-        self.backbone.eval()
+        self.backbone.train()
         for param in self.backbone.parameters():
-            param.requires_grad = False
+            param.requires_grad = True
 
         self.head = nn.Sequential(
-            nn.Dropout(0.2), 
-            nn.Linear(self.backbone_dim, 1) 
+            nn.Dropout(cfg.model.dropout),
+            nn.Linear(self.backbone_dim, 1)
         )
         
         self.apply(self._init_weights)
@@ -33,21 +33,14 @@ class MobileCLIPRanker(nn.Module):
             nn.init.orthogonal_(m.weight) 
             if m.bias is not None: nn.init.constant_(m.bias, 0)
 
-    def train(self, mode=True):
-        super().train(mode)
-        self.backbone.eval() 
-        return self
-
     def forward(self, x, valid_lens=None):
-        # x: [Batch, GroupSize, 3, H, W]
         b, g, c, h, w = x.shape
         x_flat = x.view(b * g, c, h, w)
         
-        with torch.no_grad():
-            features = self.backbone(x_flat) # [B*G, 512]
+        features = self.backbone(x_flat) 
             
-        features = features.view(b, g, -1) # [B, G, 512]
+        features = features.view(b, g, -1)
         
-        scores = self.head(features) # [B, G, 1]
+        scores = self.head(features)
         
         return scores
