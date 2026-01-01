4a2101914320b0ad252baf3c49b10265a8486472
Mithun Parab <mithunprb@gmail.com>
2026-01-01 10:12:46 +0530

    new strt

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import mobileclip
 from huggingface_hub import hf_hub_download
 import numpy as np
@@ -11,29 +10,35 @@ class MobileCLIPRanker(nn.Module):
         
         ckpt = self._download_weights(cfg.model.name)
         print(f"Loading Backbone from {ckpt}...")
+        
         model, _, _ = mobileclip.create_model_and_transforms(cfg.model.name, pretrained=ckpt)
         self.backbone = model.image_encoder
         self.backbone_dim = 512 
         
-        self.backbone.train()
+        self.backbone.eval()
         for param in self.backbone.parameters():
-            param.requires_grad = True
+            param.requires_grad = False
 
-        self.projector = nn.Sequential(
-            nn.Dropout(cfg.model.dropout),
+        self.head = nn.Sequential(
             nn.Linear(self.backbone_dim, self.backbone_dim),
             nn.LayerNorm(self.backbone_dim),
-            nn.GELU()
+            nn.GELU(),
+            nn.Dropout(cfg.model.dropout),
+            
+            nn.Linear(self.backbone_dim, self.backbone_dim // 2),
+            nn.LayerNorm(self.backbone_dim // 2),
+            nn.GELU(),
+            nn.Dropout(cfg.model.dropout),
+            
+            nn.Linear(self.backbone_dim // 2, 1)
         )
-        
-        self.ideal_vector = nn.Parameter(torch.randn(1, 1, self.backbone_dim))
-        
-        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
         
         self.apply(self._init_weights)
 
     def _download_weights(self, model_name):
-        return hf_hub_download(repo_id="apple/MobileCLIP-B", filename="mobileclip_b.pt")
+        filename = f"{model_name}.pt"
+        repo_id = "apple/MobileCLIP-B"
+        return hf_hub_download(repo_id=repo_id, filename=filename)
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -41,21 +46,14 @@ class MobileCLIPRanker(nn.Module):
             if m.bias is not None: nn.init.constant_(m.bias, 0)
 
     def forward(self, x, valid_lens=None):
-        # x: [Batch, GroupSize, 3, H, W]
         b, g, c, h, w = x.shape
         x_flat = x.view(b * g, c, h, w)
         
-        features = self.backbone(x_flat) # [B*G, 512]
-        features = features.view(b, g, -1) # [B, G, 512]
-    
+        with torch.no_grad():
+            features = self.backbone(x_flat)
+            
+        features = features.view(b, g, -1)
         
-        projected = self.projector(features) 
-        
-        projected_norm = F.normalize(projected, p=2, dim=2)
-        ideal_norm = F.normalize(self.ideal_vector, p=2, dim=2)
-        
-        similarity = (projected_norm * ideal_norm).sum(dim=2)
-        
-        scores = similarity * self.logit_scale.exp()
+        scores = self.head(features) 
         
         return scores
