9f94f1fc50def4556266157f521ab5079608960c
Mithun Parab <mithunprb@gmail.com>
2026-02-25 07:15:50 +0530

    switch to DINOv3 ConvNeXt-Large backbone

--- model.py
+++ model.py
@@ -37,8 +37,8 @@ VARIANTS = {
         "dim": 768,
         "loader": "open_clip",
     },
-    "dinov3_convnext_small": {
-        "repo": "facebook/dinov3-convnext-small-pretrain-lvd1689m",
+    "dinov3_convnext_large": {
+        "repo": "facebook/dinov3-convnext-large-pretrain-lvd1689m",
         "loader": "transformers",
     },
 }
